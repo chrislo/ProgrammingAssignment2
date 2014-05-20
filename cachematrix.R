13646496a6da840e4f7e1172b5715fbf0b3dea35
Chris Lowis <chris.lowis@gmail.com>
2014-05-20 18:53:49 +0100

    Documentation for makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
+    # Creates a data structure which caches the inverse of the matrix
+    # x
+    #
+    # Args:
+    #   x: the initial value of the matrix. No type checking is performed on the
+    #   input. Defaults to an empty matrix.
+    #
+    # Returns:
+    #   a vector of 4 functions
+    #     set(a) - sets a new value of x
+    #     get()  - returns the stored matrix
+    #     setinverse(i) - sets the value of the cached inverse of x to the inverse i
+    #     getinverse()  - returns the value of the cached inverse of x
+
     xinverse <- NULL
     set <- function(y) {
         x <<- y

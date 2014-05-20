e4052dff122c905d69f0db1cf4c147d369166a62
Chris Lowis <chris.lowis@gmail.com>
2014-05-20 18:45:32 +0100

    Implement makeCacheMatrix and cacheSolve
    I followed the examples given in the readme for a vector, substituting
    vector mean for a matrix inverse (using solve()).

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    xinverse <- NULL
+    set <- function(y) {
+        x <<- y
+        xinverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) xinverse <<- inverse
+    getinverse <- function() xinverse
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    xinverse <- x$getinverse()
+    if(!is.null(xinverse)) {
+        message("getting cached data")
+        return(xinverse)
+    }
+    data <- x$get()
+    xinverse <- solve(data)
+    x$setinverse(xinverse)
+    xinverse
 }
